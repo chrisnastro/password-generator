0a4a432f4e19cf4498b69c64e52bc959b02a074d
chrisnastro <cnastro@icloud.com>
2023-11-06 10:01:46 -0500

    added if statement to password length

--- script.js
+++ script.js
@@ -17,5 +17,15 @@ var symbol = "!@#$%^&*()?></;:[]-_"
 var number = "1234567890"
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-let length = prompt("Select password length", "8-128 characters");
+
+var passwordLength = prompt("Please select your password length (8-128 characters)"); 
+
+if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+  alert("Please select between 8-128 characters"); {
+    passwordLength = prompt("Please select your password length (8-128 characters)")
+    while (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) 
+    passwordLength = prompt("Please select your password length (8-128 characters)");
+  }
+}
+
+generateBtn.addEventListener("click", function () { writePassword(getPassword()) });;
