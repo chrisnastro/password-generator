443c04615f0664a3fe18728bb34b4e5c1eed1e8d
chrisnastro <cnastro@icloud.com>
2023-11-06 11:23:37 -0500

    added variables and if statements for alert boxes

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowercase = "abcdefghijklmnopqrstuvwxyz";
 var symbol = "!@#$%^&*()?></;:[]-_"
 var number = "1234567890"
+var includeCharacter = "";
 
 // Add event listener to generate button
 
@@ -26,6 +27,16 @@ if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
     while (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) 
     passwordLength = prompt("Please select your password length (8-128 characters)");
   }
+} else {
+  var uppercaseOption = confirm("Select 'OK' to include uppercase letters");
+  var lowercaseOption = confirm("Select 'OK' to include lowercase letters");
+  var symbolOption = confirm("Select 'OK' to include special symbols");
+  var numberOption = confirm("Select 'OK' to include numbers");
 }
+if (uppercaseOption) {includeCharacter += uppercase;}
+if (lowercaseOption) {includeCharacter += lowercase;}
+if (symbolOption) {includeCharacter += symbol;}
+if (numberOption) {includeCharaceter += number;}
+
 
 generateBtn.addEventListener("click", function () { writePassword(getPassword()) });;
