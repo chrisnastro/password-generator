d8e59bf28031598653f790e0b3e7194577b98635
chrisnastro <cnastro@icloud.com>
2023-11-06 12:45:21 -0500

    reorganized framing of javascript, got password to display in alert box

--- script.js
+++ script.js
@@ -1,42 +1,52 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  // Acceptable password variables
+ 
+  // Add event listener to generate button
 
-}
 
-// Acceptable password variables
-var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowercase = "abcdefghijklmnopqrstuvwxyz";
-var symbol = "!@#$%^&*()?></;:[]-_"
-var number = "1234567890"
-var includeCharacter = "";
+  // if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+  //   alert("Please select between 8-128 characters"); {
+  function writePassword() {
 
-// Add event listener to generate button
+    var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    var lowercase = "abcdefghijklmnopqrstuvwxyz"
+    var symbol = "!@#$%^&*()?></;:[]-_"
+    var number = "1234567890"
+    var includeCharacter = "";
+    var randomPassword = "";
+  var passwordLength = prompt("Please select your password length (8-128 characters)");
 
-var passwordLength = prompt("Please select your password length (8-128 characters)"); 
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+    alert("Please choose between 8-128 characters, reload the page, and try again");
 
-if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
-  alert("Please select between 8-128 characters"); {
-    passwordLength = prompt("Please select your password length (8-128 characters)")
-    while (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) 
-    passwordLength = prompt("Please select your password length (8-128 characters)");
+  } else {
+    var uppercaseOption = confirm("Select 'OK' to include uppercase letters");
+    var lowercaseOption = confirm("Select 'OK' to include lowercase letters");
+    var symbolOption = confirm("Select 'OK' to include special symbols");
+    var numberOption = confirm("Select 'OK' to include numbers");
   }
-} else {
-  var uppercaseOption = confirm("Select 'OK' to include uppercase letters");
-  var lowercaseOption = confirm("Select 'OK' to include lowercase letters");
-  var symbolOption = confirm("Select 'OK' to include special symbols");
-  var numberOption = confirm("Select 'OK' to include numbers");
+  if (uppercaseOption) { includeCharacter += uppercase; }
+  if (lowercaseOption) { includeCharacter += lowercase; }
+  if (symbolOption) { includeCharacter += symbol; }
+  if (numberOption) { includeCharaceter += number; }
+
+  for (i = 0; i < passwordLength; i++) {
+    randomPassword += includeCharacter[Math.floor(Math.random() * includeCharacter.length)]
+  }
+  return (randomPassword);
 }
-if (uppercaseOption) {includeCharacter += uppercase;}
-if (lowercaseOption) {includeCharacter += lowercase;}
-if (symbolOption) {includeCharacter += symbol;}
-if (numberOption) {includeCharaceter += number;}
+// var password = generatePassword();
+var passwordText = document.querySelector("#password");
+function generatePassword(randomPassword) {
 
 
-generateBtn.addEventListener("click", function () { writePassword(getPassword()) });;
+passwordText.value = randomPassword;
+
+alert(randomPassword)
+}
+
+var generateButton = document.querySelector("#generate");
+generateButton.addEventListener("click", function () { generatePassword(writePassword()) });
