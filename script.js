dc3c3fc3873b0e4e7b9a03e96308ef48563193ae
chrisnastro <cnastro@icloud.com>
2023-11-06 13:34:42 -0500

    debugged and finalized js

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
-// Assignment Code
+var passwordText = document.querySelector("#password");
 
-// Write password to the #password input
+function generatePassword(randomPassword) {
 
-  // Acceptable password variables
- 
-  // Add event listener to generate button
+  passwordText.value = randomPassword;
+  alert(randomPassword);
+}
+var generateButton = document.querySelector("#generate");
+generateButton.addEventListener("click", function () {generatePassword(writePassword())})
 
+function writePassword() {
 
-  // if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
-  //   alert("Please select between 8-128 characters"); {
-  function writePassword() {
-
-    var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    var lowercase = "abcdefghijklmnopqrstuvwxyz"
-    var symbol = "!@#$%^&*()?></;:[]-_"
-    var number = "1234567890"
-    var includeCharacter = "";
-    var randomPassword = "";
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var lowercase = "abcdefghijklmnopqrstuvwxyz"
+  var symbol = "!@#$%^&*()?></;:[]-_"
+  var number = "1234567890"
+  var includeCharacter = "";
+  var randomPassword = "";
   var passwordLength = prompt("Please select your password length (8-128 characters)");
 
   if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
-    alert("Please choose between 8-128 characters, reload the page, and try again");
+    alert("Please choose between 8-128 characters, click 'Generate Password', and try again");
 
   } else {
     var uppercaseOption = confirm("Select 'OK' to include uppercase letters");
@@ -28,25 +27,13 @@
     var symbolOption = confirm("Select 'OK' to include special symbols");
     var numberOption = confirm("Select 'OK' to include numbers");
   }
-  if (uppercaseOption) { includeCharacter += uppercase; }
-  if (lowercaseOption) { includeCharacter += lowercase; }
-  if (symbolOption) { includeCharacter += symbol; }
-  if (numberOption) { includeCharaceter += number; }
+  if (uppercaseOption) {includeCharacter += uppercase;}
+  if (lowercaseOption) {includeCharacter += lowercase;}
+  if (symbolOption) {includeCharacter += symbol;}
+  if (numberOption) {includeCharacter += number;}
 
   for (i = 0; i < passwordLength; i++) {
     randomPassword += includeCharacter[Math.floor(Math.random() * includeCharacter.length)]
   }
   return (randomPassword);
 }
-// var password = generatePassword();
-var passwordText = document.querySelector("#password");
-function generatePassword(randomPassword) {
-
-
-passwordText.value = randomPassword;
-
-alert(randomPassword)
-}
-
-var generateButton = document.querySelector("#generate");
-generateButton.addEventListener("click", function () { generatePassword(writePassword()) });
